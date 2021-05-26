051e55b2bd21f4a7fbe77a3c4f773d9ddc194c67
arorarajan <arorarajan@users.noreply.github.com>
2021-05-26 14:51:02 -0700

    Update SumNumbers.java

--- CodingPractice/src/main/java/com/arorarajan/interviewprep/codingbat/string3/SumNumbers.java
+++ CodingPractice/src/main/java/com/arorarajan/interviewprep/codingbat/string3/SumNumbers.java
@@ -18,12 +18,13 @@
 package com.arorarajan.interviewprep.codingbat.string3;
 
 /**
- * Problem Statement: http://codingbat.com/prob/p121193 Given a string, return the sum of the
- * numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more
- * digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0',
- * '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+ * Problem Statement: http://codingbat.com/prob/p121193 
+ * Given a string, return the sum of the numbers appearing in the string, ignoring all other characters.
+ * A number is a series of 1 or more digit chars in a row.
+ * Hints: Character.isDigit(char) tests if a char is one of the chars '0','1', .. '9'.
+ * Integer.parseInt(string) converts a string to an int.
  * 
- * sumNumbers("abc123xyz") -> 123
+ * sumOfNumbers("abc123xyz05def002") -> 130
  * 
  * @author Rajan Arora
  * @since Jun 14, 2014
